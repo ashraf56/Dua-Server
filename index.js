4f0bceaa83e3d04be0b16ba0734e3108e1060057
ashraf56 <ashrafulfahim07@gmail.com>
2024-02-03 21:55:45 +0600

    code updated

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
-const port = 3000
+const port = 5000
 const app = express();
 let cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
-// Open SQLite database
+//  SQLite database
 const db = new sqlite3.Database('./dua_main.sqlite');
 
 // Endpoint to get all categories
@@ -21,12 +21,15 @@ app.get('/categories', (req, res) => {
 
 // Endpoint to get all subcategories for a category
 app.get('/categories/:categoryId/subcategories', (req, res) => {
-  db.all(`SELECT * FROM sub_category WHERE cat_id = ${req.params.categoryId}`, (err, rows)  => {
-     if (err) {
-       res.status(500).send({error: err});  
-     }
-     res.status(200).json(rows); 
-  }); 
+  const catId = req.params.categoryId;
+
+  db.all('SELECT * FROM sub_category WHERE cat_id = ?', [catId], (err, rows) => {
+    if (err) {
+      return res.status(500).send({error: err});
+    }
+
+    res.status(200).json(rows);
+  });
 });
 
 
@@ -39,6 +42,23 @@ app.get('/subcategories/:subcategoryId/duas', (req, res) => {
      res.status(200).json(rows);
   });
 });
+app.get('/categories/:categoryId/dua', (req, res) => {
+  const catId = req.params.categoryId;
+
+  db.all(`SELECT * FROM dua WHERE cat_id = ${req.params.categoryId}`,(err, rows) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    if(!rows) {
+      return res.status(404).send('Dua not found');
+    }
+
+    res.json(rows);
+
+  });
+
+});
 
 app.get('/',  (req , res)  => {
 res.send('Dua started..')
@@ -46,5 +66,5 @@ res.send('Dua started..')
 
 // Start express server
 app.listen(port, () => {
-  console.log('Server started on port 3000');
+  console.log('Server started on port');
 });
